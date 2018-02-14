3562af1cee98da1b4b35ff2ffd7877b37a7dfc4a
Carson Kaucher <carsonKaucher@icloud.com>
2018-02-14 11:04:30 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
-from DigitalIO
+from digitalio import DigitalInOut, Direction, Pull
+import board
+import time
+
+motor = DigitalInOut(board.D3)
+motor.direction = Direction.OUTPUT
+
+while True:
+  motor.value = True
+  time.sleep(1)
+  motor.value = False
+  time.sleep(1)
